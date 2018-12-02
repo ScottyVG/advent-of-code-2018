1393e2f5ba22beedd2e7d3419041f8f12e65af1f
Scott Van Gilder <svangilder@gmail.com>
2018-12-01 20:08:56 -0700

    add day1 solution

--- day1/solution.py
+++ day1/solution.py
@@ -1,4 +1,12 @@
-input_data = open("./input.txt", "r")
+import itertools
 
-print (input_data)
-print (input_data.read())
+data = [int(x) for x in open("input.txt").readlines()]
+print(sum(data))
+
+freq = 0
+seen = {0}
+for num in itertools.cycle(data):
+    freq += num
+    if freq in seen:
+        print(freq); break
+    seen.add(freq)
